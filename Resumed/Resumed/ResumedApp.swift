4e1535c4ff47add420b9f12a2e24d75e258e0124
Henrique Castilho <henrique-scheer@hotmail.com>
2026-02-04 17:19:12 -0300

    Add Firebase config and init

--- Resumed/Resumed/ResumedApp.swift
+++ Resumed/Resumed/ResumedApp.swift
@@ -7,9 +7,22 @@
 
 import SwiftUI
 import Combine
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
 
 @main
 struct ResumedApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     init() {
         configureAppearance()
     }
